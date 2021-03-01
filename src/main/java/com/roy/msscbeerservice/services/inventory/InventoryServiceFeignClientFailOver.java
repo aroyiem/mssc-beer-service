3becd8f872de2e76a0f27ce2d1117b8806f84eea
Anirban Roy <aroyiem@gmail.com>
2021-03-01 14:38:32 +0000

    initial project add

---
+++ src/main/java/com/roy/msscbeerservice/services/inventory/InventoryServiceFeignClientFailOver.java
@@ -0,0 +1,21 @@
+package com.roy.msscbeerservice.services.inventory;
+
+import com.roy.msscbeerservice.services.inventory.model.BeerInventoryDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class InventoryServiceFeignClientFailOver implements InventoryServiceFeignClient {
+
+    private final InventoryFailoverFeignClient inventoryFailoverFeignClient;
+
+    @Override
+    public ResponseEntity<List<BeerInventoryDto>> getOnHandInventory(UUID beerId) {
+        return inventoryFailoverFeignClient.getOnHandInventory();
+    }
+}
